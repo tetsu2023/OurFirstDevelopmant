41d4a4d3f614fdf69dcbb13a152542ed9ec9fbe3
tetsu2023 <tennis.tetsu@icloud.com>
2023-01-14 01:11:21 +0900

    Initial Commit

---
+++ OurFirstDevelopment/OurFirstDevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurFirstDevelopmentApp.swift
+//  OurFirstDevelopment
+//
+//  Created by Tetsuo Yamaguchi on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct OurFirstDevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
