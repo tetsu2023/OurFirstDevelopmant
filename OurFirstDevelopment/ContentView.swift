822d1555553b0598c9df30424ec744edc44df58c
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-01-16 14:49:34 +0900

    「グー」「チョキ」「パ ー」の分岐を追加

--- OurFirstDevelopment/ContentView.swift
+++ OurFirstDevelopment/ContentView.swift
@@ -11,17 +11,55 @@ struct ContentView: View {
     //アラートどこかのタイミングで出そうかな
     @State var showAlert = false
     
+    @State var answerNumber = 0
+    
     var body: some View {
         VStack {
-            //画像
-            Image("gu")
-                .resizable()
-                .scaledToFit()
-            //.padding()
             
-            //テキスト
-            Text("Rock")
-                .padding()
+            //じゃんけんの数字が0の場合
+            if answerNumber == 0 {
+                Text("これからじゃんけんをします")
+            } else if answerNumber == 1 {
+                //じゃんけんの数字が1の場合、グーの画像を指定
+                
+                //画像
+                Image("gu")
+                    .resizable()
+                    .scaledToFit()
+                //.padding()
+                
+                //テキスト
+                Text("Rock")
+                    .padding()
+                
+            } else if answerNumber == 2 {
+                //じゃんけんの数字が2の場合、チョキの画像を指定
+                
+                //画像
+                Image("choki")
+                    .resizable()
+                    .scaledToFit()
+                //.padding()
+                
+                //テキスト
+                Text("scissors")
+                    .padding()
+                
+            } else if answerNumber == 3 {
+                //じゃんけんの数字が1,2以外の場合、パーの画像を指定
+                
+                //画像
+                Image("pa")
+                    .resizable()
+                    .scaledToFit()
+                //.padding()
+                
+                //テキスト
+                Text("paper")
+                    .padding()
+                
+            }
+            
             
             //button
             
